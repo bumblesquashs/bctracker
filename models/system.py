efabb3c0cb6e383402a0b700da1b7400a3519bfc
Perrin <perrinswanson@gmail.com>
2021-06-13 18:26:50 -0700

    Additional CSV improvements

--- models/system.py
+++ models/system.py
@@ -70,9 +70,6 @@ class System:
         return True
     
     # Methods for blocks
-    def add_block(self, block_id, service_id):
-        self.blocks[block_id] = Block(self, block_id, service_id)
-
     def get_block(self, block_id):
         if block_id in self.blocks:
             return self.blocks[block_id]
@@ -100,17 +97,15 @@ class System:
     def load_routes(self):
         self.routes = {}
         self.routes_by_number = {}
-        self.read_csv('routes', self.add_route)
-    
-    def add_route(self, values):
-        route_id = values['route_id']
-        number = int(values['route_short_name'])
-        name = values['route_long_name']
+        for values in self.read_csv('routes'):
+            route_id = values['route_id']
+            number = int(values['route_short_name'])
+            name = values['route_long_name']
 
-        route = Route(self, route_id, number, name)
+            route = Route(self, route_id, number, name)
 
-        self.routes[route_id] = route
-        self.routes_by_number[number] = route
+            self.routes[route_id] = route
+            self.routes_by_number[number] = route
 
     def get_route(self, route_id = None, number = None):
         if route_id is not None and route_id in self.routes:
@@ -125,31 +120,27 @@ class System:
     # Methods for services
     def load_services(self):
         self.services = {}
-        self.read_csv('calendar', self.add_service)
-        self.read_csv('calendar_dates', self.add_special_service)
+        for values in self.read_csv('calendar'):
+            service_id = values['service_id']
+            start_date = format_csv(values['start_date'])
+            end_date = format_csv(values['end_date'])
+            mon = values['monday'] == '1'
+            tue = values['tuesday'] == '1'
+            wed = values['wednesday'] == '1'
+            thu = values['thursday'] == '1'
+            fri = values['friday'] == '1'
+            sat = values['saturday'] == '1'
+            sun = values['sunday'] == '1'
 
-    def add_service(self, values):
-        service_id = values['service_id']
-        start_date = format_csv(values['start_date'])
-        end_date = format_csv(values['end_date'])
-        mon = values['monday'] == '1'
-        tue = values['tuesday'] == '1'
-        wed = values['wednesday'] == '1'
-        thu = values['thursday'] == '1'
-        fri = values['friday'] == '1'
-        sat = values['saturday'] == '1'
-        sun = values['sunday'] == '1'
+            self.services[service_id] = Service(self, service_id, start_date, end_date, mon, tue, wed, thu, fri, sat, sun)
+        for values in self.read_csv('calendar_dates'):
+            service_id = values['service_id']
+            exception_type = int(values['exception_type'])
 
-        self.services[service_id] = Service(self, service_id, start_date, end_date, mon, tue, wed, thu, fri, sat, sun)
-
-    def add_special_service(self, values):
-        service_id = values['service_id']
-        exception_type = int(values['exception_type'])
-
-        service = self.get_service(service_id)
-        if service is None or exception_type != 1:
-            return
-        service.special_service = format_csv(values['date'])
+            service = self.get_service(service_id)
+            if service is None or exception_type != 1:
+                continue
+            service.special_service = format_csv(values['date'])
 
     def get_service(self, service_id):
         if service_id in self.services:
@@ -162,20 +153,18 @@ class System:
     # Methods for shapes
     def load_shapes(self):
         self.shapes = {}
-        self.read_csv('shapes', self.add_shape)
+        for values in self.read_csv('shapes'):
+            shape_id = values['shape_id']
+            lat = float(values['shape_pt_lat'])
+            lon = float(values['shape_pt_lon'])
+            sequence = int(values['shape_pt_sequence'])
 
-    def add_shape(self, values):
-        shape_id = values['shape_id']
-        lat = float(values['shape_pt_lat'])
-        lon = float(values['shape_pt_lon'])
-        sequence = int(values['shape_pt_sequence'])
-
-        shape = self.get_shape(shape_id)
-        if shape is None:
-            shape = Shape(self, shape_id)
-            self.shapes[shape_id] = shape
-        
-        shape.add_point(lat, lon, sequence)
+            shape = self.get_shape(shape_id)
+            if shape is None:
+                shape = Shape(self, shape_id)
+                self.shapes[shape_id] = shape
+            
+            shape.add_point(lat, lon, sequence)
     
     def get_shape(self, shape_id):
         if shape_id in self.shapes:
@@ -184,45 +173,41 @@ class System:
     
     # Methods for stop times
     def load_stop_times(self):
-        self.read_csv('stop_times', self.add_stop_time)
+        for values in self.read_csv('stop_times'):
+            stop_id = values['stop_id']
+            if stop_id not in self.stops:
+                print(f'Invalid stop id: {stop_id}')
+                continue
+            trip_id = values['trip_id']
+            if trip_id not in self.trips:
+                print(f'Invalid trip id: {trip_id}')
+                continue
+            time = values['departure_time']
+            sequence = int(values['stop_sequence'])
 
-    def add_stop_time(self, values):
-        stop_id = values['stop_id']
-        if stop_id not in self.stops:
-            print(f'Invalid stop id: {stop_id}')
-            return
-        trip_id = values['trip_id']
-        if trip_id not in self.trips:
-            print(f'Invalid trip id: {trip_id}')
-            return
-        time = values['departure_time']
-        sequence = int(values['stop_sequence'])
+            stop_time = StopTime(self, stop_id, trip_id, time, sequence)
 
-        stop_time = StopTime(self, stop_id, trip_id, time, sequence)
-
-        stop_time.stop.add_stop_time(stop_time)
-        stop_time.trip.add_stop_time(stop_time)
+            stop_time.stop.add_stop_time(stop_time)
+            stop_time.trip.add_stop_time(stop_time)
 
     # Methods for stops
     def load_stops(self):
         self.stops = {}
         self.stops_by_number = {}
-        self.read_csv('stops', self.add_stop)
+        for values in self.read_csv('stops'):
+            stop_id = values['stop_id']
+            try:
+                number = int(values['stop_code'])
+            except:
+                continue
+            name = values['stop_name']
+            lat = values['stop_lat']
+            lon = values['stop_lon']
 
-    def add_stop(self, values):
-        stop_id = values['stop_id']
-        try:
-            number = int(values['stop_code'])
-        except:
-            return
-        name = values['stop_name']
-        lat = values['stop_lat']
-        lon = values['stop_lon']
+            stop = Stop(self, stop_id, number, name, lat, lon)
 
-        stop = Stop(self, stop_id, number, name, lat, lon)
-
-        self.stops[stop_id] = stop
-        self.stops_by_number[number] = stop
+            self.stops[stop_id] = stop
+            self.stops_by_number[number] = stop
 
     def get_stop(self, stop_id=None, number=None):
         if stop_id is not None and stop_id in self.stops:
@@ -235,31 +220,29 @@ class System:
     def load_trips(self):
         self.trips = {}
         self.blocks = {}
-        self.read_csv('trips', self.add_trip)
+        for values in self.read_csv('trips'):
+            trip_id = values['trip_id']
+            route_id = values['route_id']
+            if route_id not in self.routes:
+                print(f'Invalid route id: {route_id}')
+                continue
+            service_id = values['service_id']
+            if service_id not in self.services:
+                print(f'Invalid service id: {service_id}')
+                continue
+            block_id = values['block_id']
+            if block_id not in self.blocks:
+                self.blocks[block_id] = Block(self, block_id, service_id)
+            direction_id = int(values['direction_id'])
+            shape_id = values['shape_id']
+            headsign = values['trip_headsign']
 
-    def add_trip(self, values):
-        trip_id = values['trip_id']
-        route_id = values['route_id']
-        if route_id not in self.routes:
-            print(f'Invalid route id: {route_id}')
-            return
-        service_id = values['service_id']
-        if service_id not in self.services:
-            print(f'Invalid service id: {service_id}')
-            return
-        block_id = values['block_id']
-        if block_id not in self.blocks:
-            self.add_block(block_id, service_id)
-        direction_id = int(values['direction_id'])
-        shape_id = values['shape_id']
-        headsign = values['trip_headsign']
+            trip = Trip(self, trip_id, route_id, service_id, block_id, direction_id, shape_id, headsign)
 
-        trip = Trip(self, trip_id, route_id, service_id, block_id, direction_id, shape_id, headsign)
+            self.trips[trip_id] = trip
 
-        self.trips[trip_id] = trip
-
-        trip.block.add_trip(trip)
-        trip.route.add_trip(trip)
+            trip.block.add_trip(trip)
+            trip.route.add_trip(trip)
 
     def get_trip(self, trip_id):
         if trip_id in self.trips:
@@ -277,12 +260,14 @@ class System:
         for block in self.blocks.values():
             block.trips = sorted(block.trips)
 
-    def read_csv(self, name, operation):
+    def read_csv(self, name):
+        rows = []
         with open(f'./data/gtfs/{self.id}/{name}.txt', 'r') as file:
             reader = csv.reader(file)
             columns = next(reader)
             for row in reader:
-                operation(dict((k, v) for k, v in zip(columns, row)))
+                rows.append(dict((k, v) for k, v in zip(columns, row)))
+        return rows
 
 systems = {
     'victoria': System('victoria', 'victoria', 'Victoria', True),
