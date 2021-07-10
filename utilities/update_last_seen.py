d29e978b674a16573d75165cf38d32ef6999b1e1
James Ward <james@calicologic.com>
2021-07-10 15:20:00 -0700

    update last seen fix?

--- utilities/update_last_seen.py
+++ utilities/update_last_seen.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # This script takes the JSON last_seen file created
 # by the old implementation (before multiple systems
 # were added) and updates it as best as possible to
@@ -10,21 +12,31 @@ NEW_PATH = 'data/history/last_seen.json'
 
 with open(OLD_PATH, 'r') as file:
     old_data = json.load(file)
+    
+with open(NEW_PATH, 'r') as file:
+    new_data = json.load(file)['last_seen']
 
-new_data = []
-
+def key_in_new_data(key):
+    for entry in new_data:
+        if entry["number"] == int(key):
+            return True
+    return False
+    
 for key, value in old_data['last_blocks'].items():
     if key == '0' and value['blockid'] == '12345':
         continue
-    new_data.append({
-        'date': value['day'],
-        'bus_id': '',
-        'number': int(key),
-        'system_id': 'victoria',
-        'feed_version': '',
-        'block_id': value['blockid'],
-        'routes': [int(r) for r in value['routes']]
-    })
+    print('bus: ' + key)
+    if not key_in_new_data(key):
+        print('adding: ' + key)
+        new_data.append({
+            'date': value['day'],
+            'bus_id': '',
+            'number': int(key),
+            'system_id': 'victoria',
+            'feed_version': '',
+            'block_id': value['blockid'],
+            'routes': [int(r) for r in value['routes']]
+        })
 
 with open(NEW_PATH, 'w') as file:
     json.dump({ 'last_seen': new_data }, file)
