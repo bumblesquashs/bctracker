ae174a33c5e2a2c29fa946db219670e92393079d
Perrin <perrinswanson@gmail.com>
2021-12-18 17:44:11 -0800

    Backup improvements and historic backup utility

--- backup.py
+++ backup.py
@@ -4,9 +4,9 @@ import os
 import sqlite3
 from zipfile import ZipFile
 from glob import glob
-from datetime import datetime
+from datetime import datetime, timedelta
 
-CWD = os.path.dirname(__file__)
+CWD = os.path.dirname(os.path.abspath(__file__))
 
 connection = sqlite3.connect(f'{CWD}/data/bctracker.db')
 backup = sqlite3.connect(f'{CWD}/archives/bctracker.db')
@@ -14,11 +14,14 @@ connection.backup(backup)
 connection.close()
 backup.close()
 
-formatted_date = datetime.now().strftime('%Y-%m')
+date = datetime.now().replace(day=1) - timedelta(days=1)
+formatted_date = date.strftime('%Y-%m')
 
 with ZipFile(f'{CWD}/backups/{formatted_date}.zip', 'w') as zip:
-    zip.write('{CWD}/archives/bctracker.db', 'bctracker.db')
+    zip.write(f'{CWD}/archives/bctracker.db', 'bctracker.db')
     for file in glob(f'{CWD}/archives/gtfs/*_{formatted_date}-*.zip'):
         zip.write(file, file[len(f'{CWD}/archives/'):])
+        os.remove(file)
     for file in glob(f'{CWD}/archives/realtime/*_{formatted_date}-*.bin'):
         zip.write(file, file[len(f'{CWD}/archives/'):])
+        os.remove(file)
---
+++ utilities/backup.py
@@ -0,0 +1,25 @@
+import os
+from zipfile import ZipFile
+from glob import glob
+from datetime import datetime, timedelta
+from pathlib import Path
+
+CWD = os.path.dirname(Path(os.path.abspath(__file__)).parent.absolute())
+
+date = datetime.now().replace(day=1) - timedelta(days=1)
+
+while date >= datetime(2020, 1, 1):
+    formatted_date = date.strftime('%Y-%m')
+    gtfs_files = glob(f'{CWD}/archives/gtfs/*_{formatted_date}-*.zip')
+    realtime_files = glob(f'{CWD}/archives/realtime/*_{formatted_date}-*.bin')
+    
+    if len(gtfs_files) > 0 or len(realtime_files) > 0:
+        with ZipFile(f'{CWD}/backups/{formatted_date}.zip', 'w') as zip:
+            zip.write(f'{CWD}/archives/bctracker.db', 'bctracker.db')
+            for file in gtfs_files:
+                zip.write(file, file[len(f'{CWD}/archives/'):])
+                os.remove(file)
+            for file in realtime_files:
+                zip.write(file, file[len(f'{CWD}/archives/'):])
+                os.remove(file)
+    date = date.replace(day=1) - timedelta(days=1)
