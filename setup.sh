0e9b20433c26d04e39da608ca807cc9df506ef07
Perrin <perrinswanson@gmail.com>
2024-01-07 11:44:51 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Install Python dependencies
 # In environments supporting Python 2 and 3, replace pip with pip3 before running
-pip install wget
+pip install requests
 pip install Bottle
 pip install cherrypy
 pip install wsgi-request-logger
@@ -15,7 +15,6 @@ mkdir -p archives/gtfs
 mkdir -p archives/realtime
 
 mkdir -p data/gtfs
-mkdir -p data/history
 mkdir -p data/realtime
 
 mkdir -p logs
