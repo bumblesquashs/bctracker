a67c887ad5cd0d8410e2bb4eb53e850cde9bf910
Perrin Swanson <perrinswanson@gmail.com>
2026-06-22 22:58:01 -0700

    Add GTFS download table

--- repositories/impl/stop.py
+++ repositories/impl/stop.py
@@ -13,7 +13,7 @@ class StopRepository:
     
     database: Database
     
-    def create(self, context: Context, row):
+    def create(self, download_id: int, context: Context, row):
         '''Inserts a new stop into the database'''
         stop_id = row['stop_id']
         number = row['stop_code']
@@ -22,8 +22,7 @@ class StopRepository:
         self.database.insert(
             table='stop',
             values={
-                'agency_id': context.agency_id,
-                'system_id': context.system_id,
+                'download_id': download_id,
                 'stop_id': stop_id,
                 'number': number,
                 'name': row['stop_name'],
@@ -38,22 +37,27 @@ class StopRepository:
         '''Returns the stop with the given context and stop ID'''
         stops = self.database.select(
             table='stop',
-            columns=[
-                'agency_id',
-                'system_id',
-                'stop_id',
-                'number',
-                'name',
-                'lat',
-                'lon',
-                'parent_id',
-                'type'
-            ],
+            columns={
+                'download.agency_id': 'agency_id',
+                'download.system_id': 'system_id',
+                'stop.stop_id': 'stop_id',
+                'stop.number': 'number',
+                'stop.name': 'name',
+                'stop.lat': 'lat',
+                'stop.lon': 'lon',
+                'stop.parent_id': 'parent_id',
+                'stop.type': 'type'
+            },
+            joins={
+                'download': {
+                    'download.download_id': 'stop.download_id'
+                }
+            },
             filters={
-                'agency_id': context.agency_id,
-                'system_id': context.system_id,
-                'stop_id': stop_id,
-                'number': number
+                'download.agency_id': context.agency_id,
+                'download.system_id': context.system_id,
+                'stop.stop_id': stop_id,
+                'stop.number': number
             },
             limit=1,
             initializer=Stop.from_db
@@ -66,34 +70,39 @@ class StopRepository:
     def find_all(self, context: Context, stop_number: str | None = None, limit: int | None = None, lat: float | None = None, lon: float | None = None, size: float = 0.01, parent_id: str | None = None, type: StopType | None = None) -> list[Stop]:
         '''Returns all stops that match the given context'''
         filters = {
-            'agency_id': context.agency_id,
-            'system_id': context.system_id,
-            'number': stop_number,
-            'parent_id': parent_id,
-            'type': type.value if type else None
+            'download.agency_id': context.agency_id,
+            'download.system_id': context.system_id,
+            'stop.number': stop_number,
+            'stop.parent_id': parent_id,
+            'stop.type': type.value if type else None
         }
         if (lat is not None and lon is not None):
-            filters['lat'] = {
+            filters['stop.lat'] = {
                 '>=': lat,
                 '<=': lat + size
             }
-            filters['lon'] = {
+            filters['stop.lon'] = {
                 '>=': lon,
                 '<=': lon + size
             }
         return self.database.select(
             table='stop',
-            columns=[
-                'agency_id',
-                'system_id',
-                'stop_id',
-                'number',
-                'name',
-                'lat',
-                'lon',
-                'parent_id',
-                'type'
-            ],
+            columns={
+                'download.agency_id': 'agency_id',
+                'download.system_id': 'system_id',
+                'stop.stop_id': 'stop_id',
+                'stop.number': 'number',
+                'stop.name': 'name',
+                'stop.lat': 'lat',
+                'stop.lon': 'lon',
+                'stop.parent_id': 'parent_id',
+                'stop.type': 'type'
+            },
+            joins={
+                'download': {
+                    'download.download_id': 'stop.download_id'
+                }
+            },
             filters=filters,
             limit=limit,
             initializer=Stop.from_db
@@ -102,25 +111,30 @@ class StopRepository:
     def find_matches(self, context: Context, query: str) -> list[Match]:
         stops = self.database.select(
             table='stop',
-            columns=[
-                'agency_id',
-                'system_id',
-                'stop_id',
-                'number',
-                'name',
-                'lat',
-                'lon',
-                'parent_id',
-                'type'
-            ],
+            columns={
+                'download.agency_id': 'agency_id',
+                'download.system_id': 'system_id',
+                'stop.stop_id': 'stop_id',
+                'stop.number': 'number',
+                'stop.name': 'name',
+                'stop.lat': 'lat',
+                'stop.lon': 'lon',
+                'stop.parent_id': 'parent_id',
+                'stop.type': 'type'
+            },
+            joins={
+                'download': {
+                    'download.download_id': 'stop.download_id'
+                }
+            },
             filters={
-                'agency_id': context.agency_id,
-                'system_id': context.system_id,
+                'download.agency_id': context.agency_id,
+                'download.system_id': context.system_id,
                 'OR': {
-                    'number': {
+                    'stop.number': {
                         'LIKE': f'%{query}%'
                     },
-                    'name': {
+                    'stop.name': {
                         'LIKE': f'%{query}%'
                     }
                 }
@@ -134,18 +148,23 @@ class StopRepository:
         areas = self.database.select(
             table='stop',
             columns={
-                'MIN(lat)': 'min_lat',
-                'MAX(lat)': 'max_lat',
-                'MIN(lon)': 'min_lon',
-                'MAX(lon)': 'max_lon'
+                'MIN(stop.lat)': 'min_lat',
+                'MAX(stop.lat)': 'max_lat',
+                'MIN(stop.lon)': 'min_lon',
+                'MAX(stop.lon)': 'max_lon'
+            },
+            joins={
+                'download': {
+                    'download.download_id': 'stop.download_id'
+                }
             },
             filters={
-                'agency_id': context.agency_id,
-                'system_id': context.system_id,
-                'lat': {
+                'download.agency_id': context.agency_id,
+                'download.system_id': context.system_id,
+                'stop.lat': {
                     '!=': 0
                 },
-                'lon': {
+                'stop.lon': {
                     '!=': 0
                 }
             },
@@ -158,10 +177,30 @@ class StopRepository:
     
     def delete_all(self, context: Context):
         '''Deletes all stops for the given context from the database'''
+        download_ids = self.database.select(
+            table='stop',
+            columns={
+                'stop.download_id': 'download_id'
+            },
+            distinct=True,
+            joins={
+                'download': {
+                    'download.download_id': 'stop.download_id'
+                }
+            },
+            filters={
+                'download.agency_id': context.agency_id,
+                'download.system_id': context.system_id
+            },
+            initializer=lambda r: r['download_id']
+        )
+        if not download_ids:
+            return
+        if len(download_ids) == 1:
+            download_ids = download_ids[0]
         self.database.delete(
             table='stop',
             filters={
-                'agency_id': context.agency_id,
-                'system_id': context.system_id
+                'download_id': download_ids
             }
         )
--- services/impl/gtfs.py
+++ services/impl/gtfs.py
@@ -14,8 +14,10 @@ from models.block import Block
 from models.context import Context
 from models.date import Date
 from models.daterange import DateRange
+from models.download import DownloadTrigger
 from models.service import Service, ServiceException
 from models.sheet import Sheet
+from models.time import Time
 
 import repositories
 import services
@@ -26,12 +28,14 @@ class GTFSService:
     
     database: Database
     
-    def load(self, context: Context, force_download=False, update_db=False):
+    def load(self, context: Context, trigger: DownloadTrigger, force_download=False, update_db=False):
         '''Loads the GTFS for the given context into memory'''
         if not context.system.gtfs_enabled:
             return
         
         context.system.gtfs_downloaded = path.exists(f'data/gtfs/{context.system_id}')
+        if not context.system.gtfs_downloaded:
+            trigger = DownloadTrigger.MISSING
         if not context.system.gtfs_downloaded or force_download:
             self.download(context)
             update_db = True
@@ -66,7 +70,7 @@ class GTFSService:
             filter_service_ids = set()
         
         if update_db:
-            self.update_database(context, filter_service_ids)
+            self.update_database(context, trigger, filter_service_ids)
         
         services.log.info(f'Loading GTFS data for {context}')
         
@@ -118,7 +122,7 @@ class GTFSService:
             zip.extractall(data_path)
         context.system.gtfs_downloaded = True
     
-    def update_database(self, context: Context, filter_service_ids: set[str]):
+    def update_database(self, context: Context, trigger: DownloadTrigger, filter_service_ids: set[str]):
         '''Updates cached GTFS data for the given system'''
         services.log.info(f'Updating database with GTFS data for {context}')
         
@@ -128,16 +132,21 @@ class GTFSService:
         repositories.route.delete_all(context)
         repositories.point.delete_all(context)
         
-        apply_csv(context, 'routes', repositories.route.create)
-        apply_csv(context, 'stops', repositories.stop.create)
+        date = Date.today()
+        time = Time.now()
+        
+        download_id = repositories.download.create(context, date, time, trigger)
+        
+        apply_csv(download_id, context, 'routes', repositories.route.create)
+        apply_csv(download_id, context, 'stops', repositories.stop.create)
         if filter_service_ids:
-            filtered_rows = apply_csv(context, 'trips', repositories.trip.create, filter=lambda r: r['service_id'] in filter_service_ids)
+            filtered_rows = apply_csv(download_id, context, 'trips', repositories.trip.create, filter=lambda r: r['service_id'] in filter_service_ids)
             filtered_trip_ids = {r['trip_id'] for r in filtered_rows}
-            apply_csv(context, 'stop_times', repositories.departure.create, filter=lambda r: r['trip_id'] in filtered_trip_ids)
+            apply_csv(download_id, context, 'stop_times', repositories.departure.create, filter=lambda r: r['trip_id'] in filtered_trip_ids)
         else:
-            apply_csv(context, 'trips', repositories.trip.create)
-            apply_csv(context, 'stop_times', repositories.departure.create)
-        apply_csv(context, 'shapes', repositories.point.create)
+            apply_csv(download_id, context, 'trips', repositories.trip.create)
+            apply_csv(download_id, context, 'stop_times', repositories.departure.create)
+        apply_csv(download_id, context, 'shapes', repositories.point.create)
         
         self.database.commit()
     
@@ -159,7 +168,7 @@ def read_csv(context: Context, name, initializer):
         columns = next(reader)
         return [initializer(dict(zip(columns, row))) for row in reader]
 
-def apply_csv(context: Context, name, function, filter=None):
+def apply_csv(download_id: int, context: Context, name, function, filter=None):
     '''Opens a CSV file and applies a function to each row'''
     with open(f'./data/gtfs/{context.system_id}/{name}.txt', 'r', encoding='utf-8-sig') as file:
         reader = csv.reader(file)
@@ -168,7 +177,7 @@ def apply_csv(context: Context, name, function, filter=None):
         for row in reader:
             data = dict(zip(columns, row))
             if not filter or filter(data):
-                function(context, data)
+                function(download_id, context, data)
                 if filter:
                     filtered_rows.append(data)
         return filtered_rows
